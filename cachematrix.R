441348fa36149fb71ceb9f81f7520577b418d330
raymond-tao <raymond-tao@users.noreply.github.com>
2017-05-22 21:32:33 +1000

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(x = matrix()) { 
+  ## use this function to create a list of functions from a matrix
+  ## for example:  a<- makeCacheMatrix(matrix(rnorm(100),c(10,10)))
+  ## then pass it to the cacheSolve function
+  m <- NULL     
+  set <- function(y)
+  { 
+    x <<- y
+    m <<- NULL 
+  }  
+  get <- function() {x} 
+  setsolve <- function(solve) {m <<- solve}  
+  getsolve <- function(){m}
+  
+  #return a list of functions
+  list(
+    set = set, 
+    get = get,    
+    setsolve = setsolve,       
+    getsolve = getsolve) 
+} 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-   m <- NULL    
-        set <- function(y) 
-        { x <<- y    
-         m <<- NULL }    
-        get <- function() { x }    
-        setsolve <- function(solve)     { m <<- solve }    
-        getsolve <- function()    { m }    
-        list(set = set, get = get,    setsolve = setsolve,       getsolve = getsolve)
-}
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-          m <- x$getsolve()  if(!is.null(m)) {    message("getting cached data")    return(m)  }  data <- x$get()  m <- solve(data, ...)  x$setsolve(m)  m
+  ##use this function to invert the matrix 
+  ##if already exists, retrun the cached data
+  ##for example: cacheSolve(a)
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  ##otherwise, get the matrix and invert it
+  ##once inverted, the result will be stored for next time
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+
+
+
+
